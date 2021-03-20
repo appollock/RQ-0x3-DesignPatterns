e883d8855cf04d07811f3ffa24ad94024a81d2cf
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-20 14:05:37 +0100

    Initial singleton commit

---
+++ RQ-0x3-DesignPatterns/CLI.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace RQ_0x3_DesignPatterns
+{
+    public class CLI
+    {
+        private int selectedOptionIndex;
+        private ConsoleKeyInfo pressedKey;
+        public CLIMenu Menu { get; set; }
+
+        private CLI()
+        {
+            Console.CursorVisible = false;
+            selectedOptionIndex = 0;
+        }
+
+        private static CLI instance;
+
+        public static CLI GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CLI();
+            }
+            return instance;
+        }
+
+        public void WriteMenu(CLIMenu Menu, string selectedOption)
+        {
+            int cursorTop = Console.CursorTop;
+            Console.SetCursorPosition(0, 0);
+            for (int rowIndex = cursorTop; rowIndex > 0; rowIndex--)
+            {
+                Console.WriteLine(new string(' ', Console.BufferWidth));
+            }
+            Console.SetCursorPosition(0, 0);
+
+            if (!Menu.Title.Equals(""))
+                Console.WriteLine("{0}{1}", Menu.Title, Environment.NewLine);
+            if (!Menu.Description.Equals(""))
+                Console.WriteLine("{0}{1}", Menu.Description, Environment.NewLine);
+
+            for (int i = 0; i < Menu.Count; i++)
+            {
+                if (Menu.GetOptionName(i).Equals(selectedOption))
+                {
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+                Console.WriteLine(Menu.GetOptionName(i));
+            }
+        }
+
+        public string GetSelectedMenuOption()
+        {
+            WriteMenu(Menu, Menu.GetOptionName(selectedOptionIndex));
+            while (true)
+            {
+                pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                {
+                    selectedOptionIndex = (selectedOptionIndex + 1) % Menu.Count;
+                    WriteMenu(Menu, Menu.GetOptionName(selectedOptionIndex));
+                }
+                else if (pressedKey.Key == ConsoleKey.UpArrow)
+                {
+                    selectedOptionIndex = (int)((selectedOptionIndex - 1) - Menu.Count * Math.Floor((double)(selectedOptionIndex - 1) / Menu.Count));
+                    WriteMenu(Menu, Menu.GetOptionName(selectedOptionIndex));
+                }
+                else if (pressedKey.Key == ConsoleKey.Enter)
+                {
+                    return Menu.GetOptionName(selectedOptionIndex);
+                }
+            }
+        }
+
+        public string GetLine(string command = "")
+        {
+            int cursorBaseLeft = Console.CursorLeft;
+            int cursorBaseTop = Console.CursorTop;
+            if (command != "")
+                Console.Write(command);
+            string input = Console.ReadLine();
+            Console.SetCursorPosition(cursorBaseLeft, cursorBaseTop);
+            Console.Write(new string(' ', input.Length + command.Length));
+            Console.SetCursorPosition(cursorBaseLeft, cursorBaseTop);
+            return input;
+        }
+
+        public Decimal GetDecimal(string command = "")
+        {
+            Decimal value = 0;
+            while (true)
+            {
+                int cursorBaseLeft = Console.CursorLeft;
+                int cursorBaseTop = Console.CursorTop;
+                if (command != "")
+                    Console.Write(command);
+                string input = Console.ReadLine();
+                Console.SetCursorPosition(cursorBaseLeft, cursorBaseTop);
+                Console.Write(new string(' ', input.Length + command.Length));
+                Console.SetCursorPosition(cursorBaseLeft, cursorBaseTop);
+                try
+                {
+                    value = Convert.ToDecimal(input);
+                    break;
+                }
+                catch { }
+            }
+            return value;
+        }
+    }
+}
---
+++ RQ-0x3-DesignPatterns/CLIMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RQ_0x3_DesignPatterns
+{
+    public class CLIMenu
+    {
+        #region Properties and fields
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return cliOptions.Count;
+            }
+            private set
+            {
+                Count = value;
+            }
+        }
+
+        private List<string> cliOptions;
+        #endregion
+
+        #region Ctors
+        public CLIMenu()
+        {
+            Title = Description = "";
+            cliOptions = new List<string>();
+        }
+
+        public CLIMenu(string title)
+        {
+            Description = "";
+            Title = title;
+            cliOptions = new List<string>();
+        }
+
+        public CLIMenu(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            cliOptions = new List<string>();
+        }
+        #endregion
+
+        #region Methods
+        public string GetOptionName(int index)
+        {
+            if (index >= 0 && index < Count)
+                return cliOptions[index];
+            else return null;
+        }
+
+        public void Add(string cliOption)
+        {
+            cliOptions.Add(cliOption);
+        }
+
+        public void RemoveAt(int index)
+        {
+            cliOptions.RemoveAt(index);
+        }
+        #endregion
+    }
+}
